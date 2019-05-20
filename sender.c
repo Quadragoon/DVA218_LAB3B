1a24cb8868aa0ceb62b65c4f0e52f9c9353c789b
jove <jove@jove-Inspiron-5567>
2019-05-20 16:16:08 +0200

    Merged Changes, Reaf ACKs now delegated to read function

--- sender.c
+++ sender.c
@@ -51,18 +51,17 @@ struct sockaddr_in senderAddress;
 #define MIN_ACCEPTED_FRAME_SIZE 1
 #define MAX_ACCEPTED_FRAME_SIZE 65535
 
-#define MAX_MESSAGE_LENGTH 10000
+#define MAX_MESSAGE_LENGTH 20000
 
 
 //---------------------------------------------------------------------------------------------------------------
 
-int NegotiateConnection(const char* receiverIP, byte desiredWindowSize, unsigned short desiredFrameSize)
-{
+int NegotiateConnection(const char* receiverIP, byte desiredWindowSize, unsigned short desiredFrameSize) {
     DEBUGMESSAGE(5, "Negotiating connection");
 
     // memset used to make sure that the address fields are filled with '0's
-    memset(&receiverAddress, 0, sizeof(struct sockaddr_in));
-    memset(&senderAddress, 0, sizeof(struct sockaddr_in));
+    memset(&receiverAddress, 0, sizeof (struct sockaddr_in));
+    memset(&senderAddress, 0, sizeof (struct sockaddr_in));
 
     // Socket address format set to AF_INET for Internet use, 
     receiverAddress.sin_family = AF_INET;
@@ -72,101 +71,78 @@ int NegotiateConnection(const char* receiverIP, byte desiredWindowSize, unsigned
     // Get retval(return value) from inet_pton - (convert IPv4 and IPv6 addresses from text to binary form).  
     int retval = inet_pton(receiverAddress.sin_family, receiverIP, &(receiverAddress.sin_addr));
 
-    if (retval == -1)
-    {
-        CRASHWITHERROR("NegotiateConnection() failed to parse IP address");
-    }
-    else if (retval == 0)
-    {
-        DEBUGMESSAGE(0, YEL"Invalid IP address entered."RESET);
+    if (retval == -1) {
+	CRASHWITHERROR("NegotiateConnection() failed to parse IP address");
+    } else if (retval == 0) {
+	DEBUGMESSAGE(0, YEL"Invalid IP address entered."RESET);
     }
 
-    unsigned int receiverAddressLength = sizeof(receiverAddress);
-    unsigned int senderAddressLength = sizeof(senderAddress);
+    unsigned int receiverAddressLength = sizeof (receiverAddress);
+    unsigned int senderAddressLength = sizeof (senderAddress);
 
     // 'packet' struct defined in common.h
     packet packetToSend, packetBuffer;
 
     byte packetData[3];
     packetData[0] = desiredWindowSize;
-    byte* desiredFrameSizeBytes = (byte*)(&desiredFrameSize);
+    byte* desiredFrameSizeBytes = (byte*) (&desiredFrameSize);
     packetData[1] = desiredFrameSizeBytes[0];
     packetData[2] = desiredFrameSizeBytes[1];
-    WritePacket(&packetToSend, PACKETFLAG_SYN, (void*)packetData, 3, sequence);
+    WritePacket(&packetToSend, PACKETFLAG_SYN, (void*) packetData, 3, sequence);
 
     SendPacket(socket_fd, &packetToSend, &receiverAddress, receiverAddressLength);
-    if (ReceivePacket(socket_fd, &packetBuffer, &senderAddress, &senderAddressLength) != -1)
-    {
-        byte suggestedWindowSize = packetBuffer.data[0];
-        unsigned short suggestedFrameSize = ntohs((packetBuffer.data[1] * 256) + packetBuffer.data[2]);
+    if (ReceivePacket(socket_fd, &packetBuffer, &senderAddress, &senderAddressLength) != -1) {
+	byte suggestedWindowSize = packetBuffer.data[0];
+	unsigned short suggestedFrameSize = ntohs((packetBuffer.data[1] * 256) + packetBuffer.data[2]);
 
-        if (packetBuffer.flags & PACKETFLAG_SYN && packetBuffer.flags & PACKETFLAG_ACK)
-        {
-            DEBUGMESSAGE(3, "SYN+ACK: Flags "GRN"OK"RESET);
-            if (packetBuffer.sequenceNumber == (sequence))
-            {
-                DEBUGMESSAGE(3, "SYN+ACK: Sequence number "GRN"OK"RESET);
-                if (suggestedWindowSize == desiredWindowSize && suggestedFrameSize == desiredFrameSize)
-                {
-                    DEBUGMESSAGE(2, "SYN+ACK: Data "GRN"OK."RESET);
-                    windowSize = suggestedWindowSize;
-                    frameSize = suggestedFrameSize;
-                    return 1;
-                }
-                else
-                {
-                    DEBUGMESSAGE(2, "SYN+ACK: Data "RED"NOT OK."RESET);
-                    DEBUGMESSAGE(1, "SYN+ACK: Suggested parameters don't match desired ones. Data corrupted?");
-                    return -1;
-                }
-            }
-            else
-            {
-                DEBUGMESSAGE(2, "SYN+ACK: Sequence number "RED"NOT OK"RESET);
-                return -1;
-            }
-        }
-        else if (packetBuffer.flags & PACKETFLAG_SYN && packetBuffer.flags & PACKETFLAG_NAK)
-        {
-            DEBUGMESSAGE(3, "SYN+NAK: Flags "GRN"OK"RESET);
-            if (packetBuffer.sequenceNumber == (sequence))
-            {
-                DEBUGMESSAGE(3, "SYN+NAK: Sequence number "GRN"OK"RESET);
-                if (suggestedWindowSize == desiredWindowSize && suggestedFrameSize == desiredFrameSize)
-                {
-                    DEBUGMESSAGE(1, "SYN+NAK: Data "RED"VERY NOT OK."RESET);
-                    DEBUGMESSAGE(1, "SYN+NAK: Received NAK suggestion for desired parameters.\n"
-                    YEL"Something is wrong."RESET);
-                    return -1;
-                }
-                else if (suggestedWindowSize >= MIN_ACCEPTED_WINDOW_SIZE &&
-                         suggestedWindowSize <= MAX_ACCEPTED_WINDOW_SIZE &&
-                         suggestedFrameSize >= MIN_ACCEPTED_FRAME_SIZE &&
-                         suggestedFrameSize <= MAX_ACCEPTED_FRAME_SIZE)
-                {
-                    DEBUGMESSAGE(1, "SYN+NAK: Renegotiating connection...");
-                    DEBUGMESSAGE(4, "SYN+NAK: Trying again with parameters window:%d and frame:%d",
-                            suggestedWindowSize, suggestedFrameSize);
-                    return NegotiateConnection(receiverIP, suggestedWindowSize, suggestedFrameSize);
-                }
-                else
-                {
-                    DEBUGMESSAGE(2, "SYN+ACK: Data "RED"NOT OK."RESET);
-                    DEBUGMESSAGE(1, "SYN+ACK: Suggested parameters out of bounds. Connection impossible.");
-                    return -1;
-                }
-            }
-            else
-            {
-                DEBUGMESSAGE(2, "SYN+ACK: Sequence number "RED"NOT OK"RESET);
-                return -1;
-            }
-        }
-        else
-        {
-            DEBUGMESSAGE(2, "SYN+ACK: Flags "RED"NOT OK"RESET);
-            return -1;
-        }
+	if (packetBuffer.flags & PACKETFLAG_SYN && packetBuffer.flags & PACKETFLAG_ACK) {
+	    DEBUGMESSAGE(3, "SYN+ACK: Flags "GRN"OK"RESET);
+	    if (packetBuffer.sequenceNumber == (sequence)) {
+		DEBUGMESSAGE(3, "SYN+ACK: Sequence number "GRN"OK"RESET);
+		if (suggestedWindowSize == desiredWindowSize && suggestedFrameSize == desiredFrameSize) {
+		    DEBUGMESSAGE(2, "SYN+ACK: Data "GRN"OK."RESET);
+		    windowSize = suggestedWindowSize;
+		    frameSize = suggestedFrameSize;
+		    return 1;
+		} else {
+		    DEBUGMESSAGE(2, "SYN+ACK: Data "RED"NOT OK."RESET);
+		    DEBUGMESSAGE(1, "SYN+ACK: Suggested parameters don't match desired ones. Data corrupted?");
+		    return -1;
+		}
+	    } else {
+		DEBUGMESSAGE(2, "SYN+ACK: Sequence number "RED"NOT OK"RESET);
+		return -1;
+	    }
+	} else if (packetBuffer.flags & PACKETFLAG_SYN && packetBuffer.flags & PACKETFLAG_NAK) {
+	    DEBUGMESSAGE(3, "SYN+NAK: Flags "GRN"OK"RESET);
+	    if (packetBuffer.sequenceNumber == (sequence)) {
+		DEBUGMESSAGE(3, "SYN+NAK: Sequence number "GRN"OK"RESET);
+		if (suggestedWindowSize == desiredWindowSize && suggestedFrameSize == desiredFrameSize) {
+		    DEBUGMESSAGE(1, "SYN+NAK: Data "RED"VERY NOT OK."RESET);
+		    DEBUGMESSAGE(1, "SYN+NAK: Received NAK suggestion for desired parameters.\n"
+			    YEL"Something is wrong."RESET);
+		    return -1;
+		} else if (suggestedWindowSize >= MIN_ACCEPTED_WINDOW_SIZE &&
+			suggestedWindowSize <= MAX_ACCEPTED_WINDOW_SIZE &&
+			suggestedFrameSize >= MIN_ACCEPTED_FRAME_SIZE &&
+			suggestedFrameSize <= MAX_ACCEPTED_FRAME_SIZE) {
+		    DEBUGMESSAGE(1, "SYN+NAK: Renegotiating connection...");
+		    DEBUGMESSAGE(4, "SYN+NAK: Trying again with parameters window:%d and frame:%d",
+			    suggestedWindowSize, suggestedFrameSize);
+		    return NegotiateConnection(receiverIP, suggestedWindowSize, suggestedFrameSize);
+		} else {
+		    DEBUGMESSAGE(2, "SYN+ACK: Data "RED"NOT OK."RESET);
+		    DEBUGMESSAGE(1, "SYN+ACK: Suggested parameters out of bounds. Connection impossible.");
+		    return -1;
+		}
+	    } else {
+		DEBUGMESSAGE(2, "SYN+ACK: Sequence number "RED"NOT OK"RESET);
+		return -1;
+	    }
+	} else {
+	    DEBUGMESSAGE(2, "SYN+ACK: Flags "RED"NOT OK"RESET);
+	    return -1;
+	}
     }
     return 0;
 }
@@ -179,8 +155,15 @@ void* ReadPackets(ACKmngr *ACKsPointer) {
 
     ACKmngr ACKs;
     ACKs = *ACKsPointer;
-
+    packet packetBuffer;
+    unsigned int senderAddressLength = sizeof (senderAddress);    
+    
     while (KillThreads == 0) {
+	ReceivePacket(socket_fd, &packetBuffer, &senderAddress, &senderAddressLength);
+	if (packetBuffer.flags == PACKETFLAG_ACK) {
+	    ACKs.Table[ packetBuffer.sequenceNumber ] = 1;
+	    ACKs.Missing--;
+	}
 	sleep(2);
 	// TODO: Look for FINs here
     }
@@ -190,8 +173,7 @@ void* ReadPackets(ACKmngr *ACKsPointer) {
 }
 //---------------------------------------------------------------------------------------------------------------
 
-void PrintMenu()
-{
+void PrintMenu() {
     printf(YEL"--------------------------\n"RESET);
     printf(YEL"Welcome!\n"RESET);
     printf(YEL"--------------------------\n"RESET);
@@ -214,23 +196,18 @@ void LoadMessageFromFile(char readstring[MAX_MESSAGE_LENGTH]) {
     int tracker = 0;
 
     fp = fopen("message", "r");
-    if (fp != NULL)
-    {
-        while (tracker < MAX_MESSAGE_LENGTH)
-        {
-            symbol = fgetc(fp);
-            if (symbol < 0)
-            { // Check for the end of the file
-                break;
-            }
-            readstring[tracker] = (char) symbol;
-            tracker++;
-        }
-        fclose(fp);
-    }
-    else
-    {
-        DEBUGMESSAGE(1, YEL"\n -Couldn't open file 'message'- "RESET);
+    if (fp != NULL) {
+	while (tracker < MAX_MESSAGE_LENGTH) {
+	    symbol = fgetc(fp);
+	    if (symbol < 0) { // Check for the end of the file
+		break;
+	    }
+	    readstring[tracker] = (char) symbol;
+	    tracker++;
+	}
+	fclose(fp);
+    } else {
+	DEBUGMESSAGE(1, YEL"\n -Couldn't open file 'message'- "RESET);
 
     }
 }
@@ -255,59 +232,46 @@ void SlidingWindow(char* readstring, ACKmngr* ACKsPointer) {
     DEBUGMESSAGE(4, GRN"Will split over [%.2f] rounded to [ "RESET"%d"GRN" ] packets"GRN, messagedivided, packets);
     DEBUGMESSAGE(4, YEL"WindowSize is [ "RESET"%d"YEL" ] frames"RESET, windowSize);
 
-    // TODO: Implement 'Send Message' to receiver, properly ----------- // Vilken flagga används för data?
+    // TODO: Implement 'Send Message' to receiver, properly ----------- //
 
-    unsigned int receiverAddressLength = sizeof(receiverAddress);
-    unsigned int senderAddressLength = sizeof(senderAddress);
+    unsigned int receiverAddressLength = sizeof (receiverAddress);
+    unsigned int senderAddressLength = sizeof (senderAddress);
     packet* packetsToSend;
-    packetsToSend = malloc(sizeof(packet) * (windowSize + 1));
-    if (packetsToSend == NULL)
-    {
-        CRASHWITHERROR("malloc() in SlidingWindow() failed");
+    packetsToSend = malloc(sizeof (packet) * (windowSize + 1));
+    if (packetsToSend == NULL) {
+	CRASHWITHERROR("malloc() in SlidingWindow() failed");
     }
-    packet packetBuffer;
-    // 
+    
 
     for (int i = 0; i < packets; i++) {
-	if (ACKs.Missing < 1)
-    {
+	if (ACKs.Missing < windowSize) {
 
-        DEBUGMESSAGE_NONEWLINE(5, YEL"Sending:"RESET);
-        int ti = 0;
-        for (int u = MessageTracker; u < (frameSize + MessageTracker); u++)
-        { // Fill up the packet with data
-            packetsToSend[seq].data[ti] = readstring[u];
-            printf("%c", packetsToSend[seq].data[ti]);
-            ti++;
-        }
-        packetsToSend[seq].sequenceNumber = seq;
-        DEBUGMESSAGE(5, GRN"\n MessageTracker:["RESET" %d "GRN"]"RESET, MessageTracker);
-        DEBUGMESSAGE(4, GRN"\n SequenceNumber:["RESET" %d "GRN"]   seq:["RESET" %d "GRN"]\n"RESET, packetsToSend[seq].sequenceNumber, seq);
+	    DEBUGMESSAGE_NONEWLINE(5, YEL"Sending:"RESET);
+	    int trackPackData = 0;
+	    for (int u = MessageTracker; u < (frameSize + MessageTracker); u++) { // Fill up the outgoing packet with data
+		packetsToSend[seq].data[trackPackData] = readstring[u];
+		//printf("%c", packetsToSend[seq].data[trackPackData]);
+		trackPackData++;
+	    }
+	    packetsToSend[seq].sequenceNumber = seq; // Set the packet sequence number
+	    DEBUGMESSAGE(3, GRN"\n Sending Packet:["RESET" %d "GRN"]   seq:["RESET" %d "GRN"]\n"RESET, packetsToSend[seq].sequenceNumber, seq);
+	    DEBUGMESSAGE(5, GRN"\n MessageTracker:["RESET" %d "GRN"]"RESET, MessageTracker);
 
+	    if (i == packets - 1) {
+		WritePacket(&(packetsToSend[seq]), PACKETFLAG_FIN, (void*) (packetsToSend[seq].data), frameSize, seq);
+	    } else {
+		WritePacket(&(packetsToSend[seq]), 0, (void*) (packetsToSend[seq].data), frameSize,
+			seq); // Vilken flagga används för data?
+	    }
 
-        if (i == packets - 1)
-        {
-            WritePacket(&(packetsToSend[seq]), PACKETFLAG_FIN, (void*) (packetsToSend[seq].data), frameSize, seq);
-        }
-        else
-        {
-            WritePacket(&(packetsToSend[seq]), 0, (void*) (packetsToSend[seq].data), frameSize,
-                        seq); // Vilken flagga används för data?
-        }
+	    SendPacket(socket_fd, &(packetsToSend[seq]), &receiverAddress, receiverAddressLength);
 
-        SendPacket(socket_fd, &(packetsToSend[seq]), &receiverAddress, receiverAddressLength);
-
-        ACKs.Table[seq] = 0;
-        ACKs.Missing++;
-        seq++;
-        MessageTracker += frameSize;
-    }
-
-	ReceivePacket(socket_fd, &packetBuffer, &senderAddress, &senderAddressLength);
-	if (packetBuffer.flags == PACKETFLAG_ACK) {
-	    ACKs.Table[ packetBuffer.sequenceNumber ] = 1;
-	    ACKs.Missing--;
+	    ACKs.Table[seq] = 0;
+	    ACKs.Missing++;
+	    seq++;
+	    MessageTracker += frameSize;
 	}
+	
 	//usleep(5000);
     }
     //------------------------------
@@ -316,11 +280,9 @@ void SlidingWindow(char* readstring, ACKmngr* ACKsPointer) {
 }
 //---------------------------------------------------------------------------------------------------------------
 
-int main(int argc, char* argv[])
-{
-    if (argc == 2)
-    {
-        debugLevel = atoi(argv[1]);
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+	debugLevel = atoi(argv[1]);
     }
     int command = 0;
     char c;
@@ -338,39 +300,37 @@ int main(int argc, char* argv[])
 
     // Create the thread checking for FINs from the receiver------
     pthread_t thread; //Thread ID
-    pthread_create(&thread, NULL, (void*)ReadPackets, ACKsPointer);
+    pthread_create(&thread, NULL, (void*) ReadPackets, ACKsPointer);
     //------------------------------------------------------------
 
-    while (KillThreads == 0)
-    {
-        usleep(100);
+    while (KillThreads == 0) {
+	usleep(100);
 
-        system("clear"); // Clean up the console
-        LoadMessageFromFile(readstring);
-        printf("%s\n", readstring);
-        PrintMenu();
+	system("clear"); // Clean up the console
+	LoadMessageFromFile(readstring);
+	printf("%s\n", readstring);
+	PrintMenu();
 
-        scanf(" %d", &command); // Get a command from the user
-        while ((c = getchar()) != '\n' && c != EOF); //Rensar läsbufferten
+	scanf(" %d", &command); // Get a command from the user
+	while ((c = getchar()) != '\n' && c != EOF); //Rensar läsbufferten
 
-        switch (command)
-        {
-            case 1:
-                SlidingWindow(readstring, ACKsPointer);
-                break;
-            case 2:
-                // Just sending the user back to the start of the while loop
-                break;
-            case 2049:
-                // TODO: Let the receiver properly know that we are closing down the shop
-                close(socket_fd);
-                KillThreads = 1; // Make sure that we let the FINFINDER thread know that we are closing down the client
-                system("clear"); // Clean up the console
-                exit(EXIT_SUCCESS);
-                break;
-            default:
-                printf("Wrong input\n");
-        }
+	switch (command) {
+	    case 1:
+		SlidingWindow(readstring, ACKsPointer);
+		break;
+	    case 2:
+		// Just sending the user back to the start of the while loop
+		break;
+	    case 2049:
+		// TODO: Let the receiver properly know that we are closing down the shop
+		close(socket_fd);
+		KillThreads = 1; // Make sure that we let the FINFINDER thread know that we are closing down the client
+		system("clear"); // Clean up the console
+		exit(EXIT_SUCCESS);
+		break;
+	    default:
+		printf("Wrong input\n");
+	}
     }
 
     KillThreads = 1;
